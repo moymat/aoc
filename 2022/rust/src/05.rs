c465d07de7b6ffa82d5112d57eb036e826977449
moymat <deckertmichel@gmail.com>
2022-12-22 22:13:08 +0100

    2022:rust:05:part2

--- 2022/rust/src/05.rs
+++ 2022/rust/src/05.rs
@@ -4,6 +4,7 @@ use inputs::get_input;
 
 fn main() {
     part1();
+    part2();
 }
 
 #[derive(Debug)]
@@ -52,6 +53,19 @@ fn get_moves(part: &str) -> Vec<Move> {
         .collect::<Vec<Move>>()
 }
 
+fn get_all_first(crates: &Vec<Vec<Option<char>>>) -> String {
+    let mut all_first = String::new();
+    for stack_idx in 0..crates[0].len() {
+        for line in crates {
+            if let Some(char) = line[stack_idx] {
+                all_first.push(char);
+                break;
+            }
+        }
+    }
+    all_first
+}
+
 fn part1() {
     let input = get_input("05");
     let parts = input.split("\n\n").collect::<Vec<&str>>();
@@ -92,15 +106,59 @@ fn part1() {
         }
     }
 
-    let mut all_first = String::new();
-    for stack_idx in 0..crates[0].len() {
-        for line in crates.clone() {
-            if let Some(char) = line[stack_idx] {
-                all_first.push(char);
-                break;
+    let all_first = get_all_first(&crates);
+
+    println!("part1: {}", all_first);
+}
+
+fn part2() {
+    let input = get_input("05");
+    let parts = input.split("\n\n").collect::<Vec<&str>>();
+
+    let mut crates = get_crates(parts[0]);
+    let moves = get_moves(parts[1]);
+
+    for crane_move in moves {
+        let mut stack_to_move: Vec<char> = vec![];
+        for _ in 0..crane_move.amount {
+            for line in &mut crates {
+                if stack_to_move.len() >= crane_move.amount as usize {
+                    break;
+                }
+
+                match line[crane_move.start_idx] {
+                    Some(char) => {
+                        line[crane_move.start_idx] = None;
+                        stack_to_move.push(char);
+                    }
+                    None => (),
+                }
+            }
+        }
+
+        stack_to_move.reverse();
+
+        for char in stack_to_move {
+            let mut move_to: Option<usize> = None;
+            for (i, cell) in crates.iter().enumerate() {
+                match cell[crane_move.end_idx] {
+                    Some(_) => break,
+                    None => move_to = Some(i),
+                }
+            }
+
+            match move_to {
+                Some(idx) => crates[idx][crane_move.end_idx] = Some(char),
+                None => {
+                    let mut new_line = vec![None; crates[0].len()];
+                    new_line[crane_move.end_idx] = Some(char);
+                    crates.insert(0, new_line);
+                }
             }
         }
     }
 
-    println!("part1: {}", all_first);
+    let all_first = get_all_first(&crates);
+
+    println!("part2: {}", all_first);
 }
